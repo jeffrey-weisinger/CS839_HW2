f4222bfa3011191e366fcadbf23cd579d6b36eb7
Jeff <jeffreyweisinger@gmail.com>
2025-10-20 10:02:52 -0500

    Fixed Prepare.py for Wikipedia

--- nanoGPT/data/wikipedia_char/prepare.py
+++ nanoGPT/data/wikipedia_char/prepare.py
@@ -1,9 +1,3 @@
-"""
-Prepare the Shakespeare dataset for character-level language modeling.
-So instead of encoding with GPT-2 BPE tokens, we just map characters to ints.
-Will save train.bin, val.bin containing the ids, and meta.pkl containing the
-encoder and decoder and some other related info.
-"""
 import os
 import pickle
 import requests
@@ -24,14 +18,7 @@ with open("./input.txt", "w", encoding="utf-8") as f:
             f.write(valid_line)
             f.write("\n")
 
-# download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w') as f:
-        f.write(requests.get(data_url).text)
-
-with open(input_file_path, 'r') as f:
+with open('./../../wikipedia_dataset', 'r') as f:
     data = f.read()
 print(f"length of dataset in characters: {len(data):,}")
 
@@ -74,10 +61,3 @@ meta = {
 }
 with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
     pickle.dump(meta, f)
-
-# length of dataset in characters:  1115394
-# all the unique characters:
-#  !$&',-.3:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
-# vocab size: 65
-# train has 1003854 tokens
-# val has 111540 tokens
